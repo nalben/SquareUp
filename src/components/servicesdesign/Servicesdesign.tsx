6ccc03fa2e16a7b8fa71e5da4dc3a68f8615fb1a
nalben <baroqueworks64502@gmail.com>
2025-09-15 00:44:10 +0300

    сделан IconStructure, можно легко дублировать. доделать 2 компонента

--- src/components/servicesdesign/Servicesdesign.tsx
+++ src/components/servicesdesign/Servicesdesign.tsx
@@ -2,22 +2,25 @@ import React from 'react';
 import Iconsstructure from '../iconsstructure/Iconsstructure';
 import data from '@/assets/data/services/design.json'
 import Iconsblock from '../iconsstructure/iconsblock/Iconsblock';
+import classes from './Servicesdesign.module.scss'
 
 const Servicesdesign = () => {
   return (
-    <Iconsstructure
-      title="Design"
-      paragraph="At Squareup, our design team is passionate about creating stunning, user-centric designs that captivate your audience and elevate your brand. We believe that great design is not just about aesthetics; it's about creating seamless and intuitive user experiences. "
-      note="Our design services include:"
-    >
-      {data.map(group => (
-        <Iconsblock 
-          key={group.title} 
-          title={group.title} 
-          cards={group.cards} 
-        />
-      ))}
-    </Iconsstructure>
+    <div className={`${classes.cardscon} border-content-block`}>
+        <Iconsstructure
+        title="Design"
+        paragraph="At Squareup, our design team is passionate about creating stunning, user-centric designs that captivate your audience and elevate your brand. We believe that great design is not just about aesthetics; it's about creating seamless and intuitive user experiences. "
+        note="Our design services include:"
+        >
+        {data.map(group => (
+            <Iconsblock 
+            key={group.title} 
+            title={group.title} 
+            cards={group.cards} 
+            />
+        ))}
+        </Iconsstructure>
+    </div>
   );
 };
 
--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import classes from './Services.module.scss'
 import Ourservices2 from '@/components/ourservices2/Ourservices';
 import Servicesdesign from '@/components/servicesdesign/Servicesdesign';
+import Ideas from '@/components/ideas/Ideas';
 const Services = () => {
 
     return (
         <div className={classes.services}>
             <Ourservices2 />
             <Servicesdesign />
+            <Ideas />
         </div>
     );
 };
