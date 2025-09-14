c40ca8c1e6fa604190034dfaef768e6d8d2ba2b8
nalben <baroqueworks64502@gmail.com>
2025-09-14 20:57:49 +0300

    text

--- src/components/servicesdesign/Servicesdesign.tsx
+++ src/components/servicesdesign/Servicesdesign.tsx
@@ -7,7 +7,7 @@ const Servicesdesign = () => {
   return (
     <Iconsstructure
       title="Design"
-      paragraph="At Squareup, our design team..."
+      paragraph="At Squareup, our design team is passionate about creating stunning, user-centric designs that captivate your audience and elevate your brand. We believe that great design is not just about aesthetics; it's about creating seamless and intuitive user experiences. "
       note="Our design services include:"
     >
       {data.map(group => (
